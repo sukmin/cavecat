43399dbdcd19ea7bca5b59980291065b73d90c92
sukmin <killroad@gmail.com>
2015-03-12 17:24:04 +0900

    파일업로드 기능 추가

--- src/main/java/com/cavecat/board/BoardServlet.java
+++ src/main/java/com/cavecat/board/BoardServlet.java
@@ -25,19 +25,40 @@ public class BoardServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		resp.setCharacterEncoding("utf-8");
-		
-		//모든 서블릿에서 공유하는 값
+
+		// 모든 서블릿에서 공유하는 값
 		ServletContext servletContext = this.getServletContext();
 		System.out.println(servletContext.getInitParameter("movie"));
 		System.out.println(servletContext.getInitParameter("game"));
-		
-		//해당 서블릿에서만 사용할 수 있는 값
+
+		// 해당 서블릿에서만 사용할 수 있는 값
 		ServletConfig servletConfig = this.getServletConfig();
 		System.out.println(servletConfig.getInitParameter("bookStore"));
 		System.out.println(servletConfig.getInitParameter("gameSite"));
+
+		StringBuilder builder = new StringBuilder();
+		builder.append("<!doctype html>");
+		builder.append("<html lang=\"ko\">");
+		builder.append("<head><meta charset=\"utf-8\"><title>아오</title></head>");
+		builder.append("<body>");
+		builder.append("8층에는 고수가 있다.");
+		builder.append("<form action='/board/test' method='post' enctype='multipart/form-data'>");
+		builder.append("<input type='text' name='testText'><br>");
+		builder.append("<input type='file' name='testFile'><br>");
+		builder.append("<input type='submit'><br>");
+		builder.append("</form>");
+		builder.append("</body>");
+		builder.append("</html>");
+
+		resp.getWriter().println(builder.toString());
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		System.out.println(req.getParameter("testText"));
+		System.out.println(req.getPart("testFile").getName());
 		
-		String html = "<!doctype html><html lang=\"ko\"><head><meta charset=\"utf-8\"><title>%s</title></head><body>%s</body></html>";
-		resp.getWriter().println(String.format(html, "무균실", "4층 무균실에는 뭔가 특별한 것이 있다."));
 	}
 
 }
