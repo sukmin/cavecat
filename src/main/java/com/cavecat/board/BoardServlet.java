540c7beed28f0bf06a3925bc81494d9ff3bb5ac4
sukmin <killroad@gmail.com>
2015-03-12 16:02:47 +0900

    - 서블릿 컨텍스트파람 및 이닛파람 테스트

--- src/main/java/com/cavecat/board/BoardServlet.java
+++ src/main/java/com/cavecat/board/BoardServlet.java
@@ -2,6 +2,8 @@ package com.cavecat.board;
 
 import java.io.IOException;
 
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -23,6 +25,16 @@ public class BoardServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		resp.setCharacterEncoding("utf-8");
+		
+		//모든 서블릿에서 공유하는 값
+		ServletContext servletContext = this.getServletContext();
+		System.out.println(servletContext.getInitParameter("movie"));
+		System.out.println(servletContext.getInitParameter("game"));
+		
+		//해당 서블릿에서만 사용할 수 있는 값
+		ServletConfig servletConfig = this.getServletConfig();
+		System.out.println(servletConfig.getInitParameter("bookStore"));
+		System.out.println(servletConfig.getInitParameter("gameSite"));
 		
 		String html = "<!doctype html><html lang=\"ko\"><head><meta charset=\"utf-8\"><title>%s</title></head><body>%s</body></html>";
 		resp.getWriter().println(String.format(html, "무균실", "4층 무균실에는 뭔가 특별한 것이 있다."));
