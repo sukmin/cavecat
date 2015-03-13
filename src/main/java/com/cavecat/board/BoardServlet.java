87031d1290b2e4e8d22a745bdf34d7a66c5a1355
sukmin <killroad@gmail.com>
2015-03-13 15:07:22 +0900

    println 을 jsp로 변경

--- src/main/java/com/cavecat/board/BoardServlet.java
+++ src/main/java/com/cavecat/board/BoardServlet.java
@@ -2,9 +2,11 @@ package com.cavecat.board;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -36,21 +38,11 @@ public class BoardServlet extends HttpServlet {
 		System.out.println(servletConfig.getInitParameter("bookStore"));
 		System.out.println(servletConfig.getInitParameter("gameSite"));
 
-		StringBuilder builder = new StringBuilder();
-		builder.append("<!doctype html>");
-		builder.append("<html lang=\"ko\">");
-		builder.append("<head><meta charset=\"utf-8\"><title>아오</title></head>");
-		builder.append("<body>");
-		builder.append("8층에는 고수가 있다.");
-		builder.append("<form action='/board/test' method='post' enctype='multipart/form-data'>");
-		builder.append("<input type='text' name='testText'><br>");
-		builder.append("<input type='file' name='testFile'><br>");
-		builder.append("<input type='submit'><br>");
-		builder.append("</form>");
-		builder.append("</body>");
-		builder.append("</html>");
-
-		resp.getWriter().println(builder.toString());
+		
+		RequestDispatcher requestDispatcher = req.getRequestDispatcher("/WEB-INF/jsp/board/file.jsp");
+		requestDispatcher.forward(req, resp);
+		
+		
 	}
 
 	@Override
---
+++ src/main/webapp/WEB-INF/jsp/board/file.jsp
@@ -0,0 +1,14 @@
+<%@ page session="false" %>
+<!doctype html>
+<html>
+<head>
+<head><meta charset="utf-8"><title>아오</title></head>
+<body>
+8층에는 고수가 있다.
+<form action="/board/test" method="post" enctype="multipart/form-data">
+<input type="text" name="testText"><br>
+<input type="file" name="testFile"><br>
+<input type='submit'><br>
+</form>
+</body>
+</html>
