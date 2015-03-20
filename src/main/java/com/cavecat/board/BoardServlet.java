efbbafc719fff05e596f1bbac6edc0fd6b3b4ba7
sukmin <killroad@gmail.com>
2015-03-20 17:33:55 +0900

    - 포매터 적용 - 로그인되어있지 않으면 board에 접근하지 못하도록 함

--- src/main/java/com/cavecat/board/BoardServlet.java
+++ src/main/java/com/cavecat/board/BoardServlet.java
@@ -12,49 +12,64 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 public class BoardServlet extends HttpServlet {
-  private static final long serialVersionUID = 4251285399811752982L;
+	private static final long serialVersionUID = 4251285399811752982L;
 
-  @Override
-  public void destroy() {
-    System.out.println("Servlet " + this.getServletName() + " has stopped.");
-  }
+	@Override
+	public void destroy() {
+		System.out.println("Servlet " + this.getServletName() + " has stopped.");
+	}
 
-  @Override
-  public void init() throws ServletException {
-    System.out.println("Servlet " + this.getServletName() + " has started.");
-  }
+	@Override
+	public void init() throws ServletException {
+		System.out.println("Servlet " + this.getServletName() + " has started.");
+	}
 
-  @Override
-  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
-      IOException {
-    resp.setCharacterEncoding("utf-8");
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-    // 모든 서블릿에서 공유하는 값
-    ServletContext servletContext = this.getServletContext();
-    System.out.println(servletContext.getInitParameter("movie"));
-    System.out.println(servletContext.getInitParameter("game"));
+		HttpSession session = req.getSession();
+		
+		// 로그인이 되어있지 않은 경우
+		if (session.getAttribute("id") == null) {
+			resp.sendRedirect("/board/login");
+			return;
+		}
 
-    // 해당 서블릿에서만 사용할 수 있는 값
-    ServletConfig servletConfig = this.getServletConfig();
-    System.out.println(servletConfig.getInitParameter("bookStore"));
-    System.out.println(servletConfig.getInitParameter("gameSite"));
+		resp.setCharacterEncoding("utf-8");
 
-    HttpSession session = req.getSession();
-    session.setAttribute("apple", "jobs");
-    System.out.println(session.getId());
+		// 모든 서블릿에서 공유하는 값
+		ServletContext servletContext = this.getServletContext();
+		System.out.println(servletContext.getInitParameter("movie"));
+		System.out.println(servletContext.getInitParameter("game"));
 
-    RequestDispatcher requestDispatcher = req.getRequestDispatcher("/WEB-INF/jsp/board/file.jsp");
-    requestDispatcher.forward(req, resp);
-  }
+		// 해당 서블릿에서만 사용할 수 있는 값
+		ServletConfig servletConfig = this.getServletConfig();
+		System.out.println(servletConfig.getInitParameter("bookStore"));
+		System.out.println(servletConfig.getInitParameter("gameSite"));
 
-  @Override
-  protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
-      IOException {
-    System.out.println(req.getParameter("testText"));
-    System.out.println(req.getPart("testFile").getName());
-    System.out.println(req.getPart("testFile").getSubmittedFileName());
+		session.setAttribute("apple", "jobs");
+		System.out.println(session.getId());
 
-    RequestDispatcher requestDispatcher = req.getRequestDispatcher("/WEB-INF/jsp/board/file.jsp");
-    requestDispatcher.forward(req, resp);
-  }
+		RequestDispatcher requestDispatcher = req.getRequestDispatcher("/WEB-INF/jsp/board/file.jsp");
+		requestDispatcher.forward(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		HttpSession session = req.getSession();
+		
+		// 로그인이 되어있지 않은 경우
+		if (session.getAttribute("id") == null) {
+			resp.sendRedirect("/board/login");
+			return;
+		}
+		
+		System.out.println(req.getParameter("testText"));
+		System.out.println(req.getPart("testFile").getName());
+		System.out.println(req.getPart("testFile").getSubmittedFileName());
+
+		RequestDispatcher requestDispatcher = req.getRequestDispatcher("/WEB-INF/jsp/board/file.jsp");
+		requestDispatcher.forward(req, resp);
+	}
 }
--- src/main/java/com/cavecat/board/LoginServlet.java
+++ src/main/java/com/cavecat/board/LoginServlet.java
@@ -12,78 +12,77 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 public class LoginServlet extends HttpServlet {
-  private static final long serialVersionUID = 9222155466231723860L;
-  private static final Map<String, String> userData = new HashMap<>();
+	private static final long serialVersionUID = 9222155466231723860L;
+	private static final Map<String, String> userData = new HashMap<>();
 
-  static {
-    userData.put("serivires", "serivires");
-    userData.put("killroad", "killroad");
-  }
+	static {
+		userData.put("serivires", "serivires");
+		userData.put("killroad", "killroad");
+	}
 
-  @Override
-  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
-      IOException {
-    // <Manager className="org.apache.catalina.session.PersistentManager" saveOnRestart="true"/>
-    //
-    // Tomcat 서버를 재시동하거나 심지어 컴퓨터를 껐다 켜도 기존의 세션이 백업이 되는 기능.
-    // 일정시간 사용되지 않는 세션은 하드에 저장되어서 메모리를 차지하지 않게 됩니다.
-    // 그러다 다시 그 세션의 요청이 들어오면 복원되어서 사용이 됩니다.
-    // 톰캣을 셧다운할 때에 현재 있는 모든 세션이 저장됩니다.
-    // 기본설정은 true.
-    HttpSession session = req.getSession();
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// <Manager className="org.apache.catalina.session.PersistentManager"
+		// saveOnRestart="true"/>
+		//
+		// Tomcat 서버를 재시동하거나 심지어 컴퓨터를 껐다 켜도 기존의 세션이 백업이 되는 기능.
+		// 일정시간 사용되지 않는 세션은 하드에 저장되어서 메모리를 차지하지 않게 됩니다.
+		// 그러다 다시 그 세션의 요청이 들어오면 복원되어서 사용이 됩니다.
+		// 톰캣을 셧다운할 때에 현재 있는 모든 세션이 저장됩니다.
+		// 기본설정은 true.
+		HttpSession session = req.getSession();
 
-    // 로그인이 이미 되어있는 경우
-    if (session.getAttribute("id") != null) {
+		// 로그인이 이미 되어있는 경우
+		if (session.getAttribute("id") != null) {
 
-      System.out.println("아이디 : " + session.getAttribute("id"));
-      resp.sendRedirect("/board/board");
-      return;
-    }
+			System.out.println("아이디 : " + session.getAttribute("id"));
+			resp.sendRedirect("/board/board");
+			return;
+		}
 
-    req.setAttribute("loginFailed", true);
-    RequestDispatcher requestDispatcher = req.getRequestDispatcher("/WEB-INF/jsp/board/login.jsp");
-    requestDispatcher.forward(req, resp);
-  }
+		req.setAttribute("loginFailed", true);
+		RequestDispatcher requestDispatcher = req.getRequestDispatcher("/WEB-INF/jsp/board/login.jsp");
+		requestDispatcher.forward(req, resp);
+	}
 
-  @Override
-  protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
-      IOException {
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-    HttpSession session = req.getSession();
+		HttpSession session = req.getSession();
 
-    // 로그인이 이미 되어있는 경우
-    if (session.getAttribute("id") != null) {
-      resp.sendRedirect("/board/board");
-      return;
-    }
+		// 로그인이 이미 되어있는 경우
+		if (session.getAttribute("id") != null) {
+			resp.sendRedirect("/board/board");
+			return;
+		}
 
-    String id = req.getParameter("id");
-    String passwd = req.getParameter("passwd");
+		String id = req.getParameter("id");
+		String passwd = req.getParameter("passwd");
 
-    // 로그인 정보
-    if (isLoginFail(id, passwd)) {
-      req.setAttribute("loginFailed", true);
-      req.getRequestDispatcher("/WEB-INF/jsp/board/login.jsp").forward(req, resp);
-    } else {
-      session.setAttribute("id", id);
+		// 로그인 정보
+		if (isLoginFail(id, passwd)) {
+			req.setAttribute("loginFailed", true);
+			req.getRequestDispatcher("/WEB-INF/jsp/board/login.jsp").forward(req, resp);
+		} else {
+			session.setAttribute("id", id);
 
-      System.out.println(" 세션아이디 전  :" + session.getId());
-      // 세션 고정 공격 방지 http://storyj.net/?p=52
-      req.changeSessionId();
-      System.out.println(" 세션아이디 후   :" + session.getId());
-      resp.sendRedirect("/board/board");
-    }
-  }
+			System.out.println(" 세션아이디 전  :" + session.getId());
+			// 세션 고정 공격 방지 http://storyj.net/?p=52
+			req.changeSessionId();
+			System.out.println(" 세션아이디 후   :" + session.getId());
+			resp.sendRedirect("/board/board");
+		}
+	}
 
-  private boolean isLoginFail(String id, String passwd) {
-    if (id == null || passwd == null) {
-      return true;
-    }
+	private boolean isLoginFail(String id, String passwd) {
+		if (id == null || passwd == null) {
+			return true;
+		}
 
-    if (userData.containsKey(id) == false || passwd.equals(userData.get(id)) == false) {
-      return true;
-    }
+		if (userData.containsKey(id) == false || passwd.equals(userData.get(id)) == false) {
+			return true;
+		}
 
-    return false;
-  }
+		return false;
+	}
 }
