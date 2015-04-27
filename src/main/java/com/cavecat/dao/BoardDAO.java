f5702e22f56f1991bbc0f1da205ddc61fb548476
serivires <serivires@gmail.com>
2015-04-28 00:28:37 +0900

    - 코드 수정중...

--- src/main/java/com/cavecat/dao/BoardDAO.java
+++ src/main/java/com/cavecat/dao/BoardDAO.java
@@ -2,18 +2,57 @@ package com.cavecat.dao;
 
 import java.util.List;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Order;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import com.cavecat.model.Board;
 
-public interface BoardDAO {
+/**
+ * 참고 : http://netframework.tistory.com/353 <br>
+ * 참고 : https://www.yammacode.com/view?id=122
+ * 
+ * @author sukmin
+ *
+ */
+@Component
+public class BoardDAO {
 
-  public List<Board> selectAll();
+  @Autowired
+  private SessionFactory sessionFactory;
 
-  public Board select(Long sequence);
+  @SuppressWarnings("unchecked")
+  public List<Board> selectAll() {
+    Session session = sessionFactory.openSession();
 
-  public Long insert(Board board);
+    return (List<Board>) session.createCriteria(Board.class) //
+        .addOrder(Order.desc("sequence")) //
+        .list();
+  }
 
-  public Board update(Board board);
+  public Board select(Long sequence) {
+    Session session = sessionFactory.openSession();
+    return (Board) session.get(Board.class, sequence);
+  }
 
-  public void delete(Board board);
+  public Long insert(Board board) {
+    Session session = sessionFactory.openSession();
+    session.save(board);
+    return board.getSequence();
+  }
 
+  public Board update(Board board) {
+    Session session = sessionFactory.openSession();
+    session.update(board);
+    session.flush();
+    return board;
+  }
+
+  public void delete(Board board) {
+    Session session = sessionFactory.openSession();
+    session.delete(board);
+    session.flush();
+  }
 }
